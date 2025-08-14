fdc60aa2abd32b6a5355de0b88e586f602a1483e
Nino Bergen <nino.bergen@icloud.com>
2025-08-15 00:14:36 +0200

    refactor: update admin sidebar

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -196,7 +196,7 @@ const Sidebar = React.forwardRef<
           <SheetContent
             data-sidebar="sidebar"
             data-mobile="true"
-            className="w-[--sidebar-width] bg-sidebar p-0 text-sidebar-foreground [&>button]:hidden"
+            className="w-[--sidebar-width] !bg-sidebar p-0 text-sidebar-foreground [&>button]:hidden z-50 border-none"
             style={
               {
                 "--sidebar-width": SIDEBAR_WIDTH_MOBILE,
